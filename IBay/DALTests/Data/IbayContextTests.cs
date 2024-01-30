f84f0b17ece38ee367f78c80cda0c1db369db5b4
Lcd789 <122019230+Lcd789@users.noreply.github.com>
2024-01-30 16:15:20 +0100

    Dépendance pour les tests unitaires installée

---
+++ IBay/DALTests/Data/IbayContextTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data.Tests
+{
+    [TestClass()]
+    public class IbayContextTests
+    {
+        [TestMethod()]
+        public void CreateUserTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
